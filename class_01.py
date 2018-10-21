32a2461771352e52613be03628ee7b560c2ce586
RohiShar <rohit.sharma@gmail.com>
2018-10-21 11:57:04 +0400

    This is initial commit

---
+++ class_01.py
@@ -0,0 +1,11 @@
+"""""
+This is class 01 file
+
+1)we set up python3.7, git, pycharm
+2) create github repo for batch
+3) created virtual envirnment
+4) setup pycharm project
+5) created this file and greeted entirer world..:)
+
+"""
+print("Hello World")
